d8beee58b0d5f3be6b798cd157ea69d4b6599f3e
Ankitarjput07 <ankitrajput15557@gmail.com>
2026-05-29 11:57:32 +0530

    chore: support dynamic VITE_API_URL environment variable for production hosting

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App.jsx";
 import "./index.css";
+import axios from "axios";
+
+// Configure default API endpoint dynamically
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
